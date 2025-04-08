21c36cca5c45e3f42f435f27d569d584c2fc6d03
notnbhd <bachhaidangnguyen@gmail.com>
2025-04-08 22:22:53 +0700

    implement into a webapp

---
+++ src/app.py
@@ -0,0 +1,41 @@
+import io
+import base64
+from PIL import Image
+from flask import Flask, render_template, request, url_for
+
+from network import image_classify
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'image' not in request.files:
+        return 'No file passed'
+    
+    file = request.files['image']
+
+    if file:
+        # Read the image
+        img_data = file.read()
+        img = Image.open(io.BytesIO(img_data))
+        
+        # Get the prediction
+        prediction = image_classify(img)
+        
+        # Create a data URL for the image to display in the result page
+        buffered = io.BytesIO()
+        img.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue()).decode()
+        image_data_url = f"data:image/jpeg;base64,{img_str}"
+        
+        # Render the result template
+        return render_template('result.html', 
+                              prediction=prediction,
+                              image_path=image_data_url)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ src/network.py
@@ -0,0 +1,32 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import os
+
+
+
+def image_classify(img):
+    model = torchvision.models.efficientnet_b0(weights='DEFAULT')
+    class_names = ['sushi', 'pizza', 'steak']
+    model.classifier = torch.nn.Sequential(
+        torch.nn.Dropout(p=0.2, inplace=True),
+        torch.nn.Linear(in_features=1280, out_features=len(class_names), bias=True)
+        )
+    model.load_state_dict(torch.load('./src/best_model.pth'))
+
+    image_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225])
+        ])
+
+    img = image_transform(img)
+    img = img.unsqueeze(0)
+
+    model.eval()
+    with torch.no_grad():
+        output = model(img)
+        _, pred = torch.max(output, 1)
+    
+    return class_names[pred.item()]
